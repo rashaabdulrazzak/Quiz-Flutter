cd36debe27cf0910a23a91a646c4efbe1da2041a
rashaabdulrazzak <rasha.abdulrazzak.com>
2023-02-01 12:21:47 +0300

    quiz with true and false

---
+++ android/app/src/main/kotlin/com/example/quizller_mine/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizller_mine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/qusetion.dart
@@ -0,0 +1,10 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+  // old way
+  // Question(String q, bool a ){
+  //   questionText = q;
+  //   questionAnswer = a;
+  // }
+  Question(this.questionText,this.questionAnswer);
+}
